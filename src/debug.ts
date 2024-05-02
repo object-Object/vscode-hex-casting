160b9bf698540b7c8e488c6911acd946086a119a
object-Object <object@objectobject.ca>
2024-05-02 19:31:16 -0400

    Update for latest HexDebug

--- src/debug.ts
+++ src/debug.ts
@@ -1,11 +1,17 @@
 import * as vscode from "vscode";
 
+// not exhaustive
+interface HexDebugLaunchArgs extends vscode.DebugConfiguration {
+    port?: number;
+}
+
 class ServerDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
     createDebugAdapterDescriptor(
         session: vscode.DebugSession,
         executable: vscode.DebugAdapterExecutable | undefined,
     ): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
-        return new vscode.DebugAdapterServer(4444);
+        let launchArgs = session.configuration as HexDebugLaunchArgs;
+        return new vscode.DebugAdapterServer(launchArgs.port ?? 4444);
     }
 }
 
